8a7172c8dbfc89747446d0cda8df965fae082b55
Pablo <pdramirez.h@gmail.com>
2022-03-10 21:23:35 -0500

    DefaultProps

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -3,21 +3,24 @@ import React from 'react';
 import PropTypes from "prop-types";
 // uso de destructuracion de las props 
 
-const PrimeraApp = ({ saludo }) => {
+const PrimeraApp = ({ saludo, subtitulo }) => {
 
 
 
     return (
         <>
             <h1>{saludo}</h1>
-            <p>Soy un parrafo!!!</p>
+            <p>{subtitulo}</p>
         </>
     );
 }
-
+// PropTypes
 PrimeraApp.propTypes = {
     saludo: PropTypes.string.isRequired,
-
+}
+// DefaultProps
+PrimeraApp.defaultProps = {
+    subtitulo: 'Soy un subtitulo'
 }
 export default PrimeraApp;
 
