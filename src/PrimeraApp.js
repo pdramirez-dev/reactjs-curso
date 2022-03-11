b072f0f6bf14e4cd64e9a41ba7cc5727f082b8bc
Pablo <pdramirez.h@gmail.com>
2022-03-10 20:54:40 -0500

    Renderizado de variables

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -2,9 +2,17 @@
 import React from 'react';
 
 const PrimeraApp = () => {
+
+    const saludo = 'Hola Mundo';
+
+    const objeto = {
+        nombre: 'Pablo',
+        edad: 24
+    };
     return (
         <>
-            <h1>Hola Mundo</h1>
+            <h1>{saludo}</h1>
+            <pre>{JSON.stringify(objeto, null, 3)}</pre>
             <p>Soy un parrafo!!!</p>
         </>
     );
