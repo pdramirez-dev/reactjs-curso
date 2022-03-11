517d48a72c374ae54917137864e14cf0474fe76f
Pablo <pdramirez.h@gmail.com>
2022-03-10 20:10:44 -0500

    Primer App

---
+++ src/index.js
@@ -0,0 +1,8 @@
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+const saludos = <h1>Hola Mundo</h1>;
+const divRoot = document.querySelector('#root');
+
+ReactDOM.render(saludos, divRoot);
