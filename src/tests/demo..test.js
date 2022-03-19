3a5ff44cd32ede6e76b357427d96d00607fb0781
Pablo <pdramirez.h@gmail.com>
2022-03-18 21:40:43 -0400

    update demo.test.js

--- src/tests/demo..test.js
+++ src/tests/demo..test.js
@@ -1,7 +1,11 @@
 
-test('Deve ser True', () => {
-    const isActive = true;
-    if(!isActive){
-        throw new Error('No esta Activo')
-    }
+describe('Pruebas en el archivo demo.test.js', () => {
+    test('Deve ser Iguales los mensajes', () => {
+        // 1. inicializacion
+        const mensaje = "Hola Mundo";
+        // 2. estimulo
+        const mensaje2 = `Hola Mundo`;
+        //  3 assert
+        expect(mensaje).toBe(mensaje2);
+    });
 });
